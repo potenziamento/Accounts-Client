ec478c2ca2666ebc5905cec2a8e54f0c99c9d1a8
LorenzoCabras <cabras.lor@gmail.com>
2023-10-31 17:09:10 +0100

    fix

--- src/app/money-transfer/money-transfer.component.ts
+++ src/app/money-transfer/money-transfer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-money-transfer',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./money-transfer.component.css']
 })
 export class MoneyTransferComponent {
+  transferData = {
+    creditorName: '',
+    creditorAccountCode: '',
+    description: '',
+    currency: 'EUR',
+    amount: '',
+    executionDate: ''
+  };
+  successMessage: string | null = null;
+  errorMessage: string | null = null;
 
+  constructor(private apiService: ApiService) { }
+
+  submitTransfer(): void {
+    this.apiService.createMoneyTransfer(this.transferData).subscribe(
+      response => {
+        this.successMessage = 'Trasferimento completato con successo!';
+        this.errorMessage = null;
+      },
+      error => {
+        this.errorMessage = 'Si è verificato un errore durante il trasferimento.';
+        this.successMessage = null;
+        console.error(error);
+      }
+    );
+  }
 }
