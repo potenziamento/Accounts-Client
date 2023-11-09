6d7c9831e95aec15f2a2ebf8cb5a8e7e69ee453f
LorenzoCabras <cabras.lor@gmail.com>
2023-11-09 08:54:48 +0100

    Fixed CSS and TS - components balance/transactions + app.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
+import { MatNativeDateModule } from '@angular/material/core'
+import { MatDatepickerModule } from '@angular/material/datepicker'
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 
 
 @NgModule({
@@ -25,7 +29,10 @@ import { MatCardModule } from '@angular/material/card';
     BrowserAnimationsModule,
     HttpClientModule,
     FormsModule,
-    MatCardModule
+    MatCardModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/transactions/transactions.component.ts
+++ src/app/transactions/transactions.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
 
+
 @Component({
   selector: 'app-transactions',
   templateUrl: './transactions.component.html',
@@ -10,24 +11,38 @@ export class TransactionsComponent implements OnInit{
 
   transactions: any[] = [];
   error: string | null = null;
-  fromDate: string = '2019-01-01';
-  toDate: string = '2019-12-01';
+  fromDate: Date = new Date('2019-01-01');
+  toDate: Date = new Date('2019-12-01');
+  
 
   constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
     this.getTransactions();
   }
-  getTransactions():void{
-    this.apiService.getTransactions(this.fromDate, this.toDate).subscribe(
-      data => {
-        this.transactions = data;
-        this.error = null;
-      },
-      err => {
-        this.error = 'Si è verificato un errore nel recupero delle transazioni.';
-        console.error(err);
-      }
-    );
+  getTransactions(): void {
+    let fromDateString: string = '';
+    let toDateString: string = '';
+  
+    if (this.fromDate && this.toDate) {
+      const fromISO = this.fromDate.toISOString();
+      fromDateString = fromISO.split('T')[0];
+      const toISO = this.toDate.toISOString();
+      toDateString = toISO.split('T')[0];
+  
+      this.apiService.getTransactions(fromDateString, toDateString).subscribe(
+        data => {
+          this.transactions = data;
+          this.error = null;
+        },
+        err => {
+          this.error = 'Si è verificato un errore nel recupero delle transazioni.';
+          console.error(err);
+        }
+      );
+    } else {
+      this.transactions = [];
+      this.error = null;
+    }
   }
 }
